70d63a211ce5b9b0e78af7d724645f0128f06a50
Programnik <41002203+Programnik@users.noreply.github.com>
2020-01-29 20:35:07 +0100

    Changed method of scanning image: list(.getdata()); char_values file contains RegEx of each image which are now deleted since this alternative is better; processed images will be cut out so only date bar is processed

--- folderpicker.py
+++ folderpicker.py
@@ -5,11 +5,15 @@ def browse_button():
     global folder_path
     filename = filedialog.askdirectory() # make so this starts off in a Darkest Hour folder
     folder_path.set(filename)
-       
+
+def commence(cur_folder):  # Window must be destroyed
+    walk_folder(cur_folder+ "/")
+    window.destroy()  
+
 window = Tk()
 folder_path = StringVar()
 
-confirm = Button(text="Select", command=lambda: walk_folder(folder_path.get()))
+confirm = Button(text="Select", command=lambda:commence(folder_path.get()))  # seems like first file is extracted too
 confirm.grid(row=0, column=1)
 browser = Button(text="Browse", command=browse_button)
 browser.grid(row=0,column=2)
--- image_fixer.py
+++ image_fixer.py
@@ -1,47 +1,43 @@
 from PIL import Image
 import os
-#from os import listdir, path
-
-def cut_img (img:Image)->Image:
-    width, height = img.size
-    return img.crop((259, 76, width, height))  # cuts out everything except map
+from char_values import chars_lite, chars_full
 
 def get_date(cur_img:Image)->list:
     color = cur_img.convert("RGB").getpixel((0, 48)) # color of first pixel under the upper black seperator line above the bar
     if color == (96, 96, 88): 
-        cur_img_bytes = cur_img.tobytes()
-        return iterate_chars("dhfull_chars") 
+        cur_img_data = list(cur_img.crop(821,53, 946, 63).getdata()) # everything but date bar is cut out
+        return iterate_chars(1) 
     elif color == (184, 180, 152): 
-        cur_img_bytes = cur_img.tobytes()
-        return iterate_chars("dhlite_chars")
+        cur_img_data = list(cur_img.crop(800, 53, 925, 63).getdata())  # not sure if this adequately cuts out all of the needed information
+        return iterate_chars()
     else: return None  
     
-    def iterate_chars(char_folder:str)->list:
-        nonlocal cur_img_bytes
+    def iterate_chars(is_full:bool=False)->list:  
         nonlocal cur_img
-
-        temp_img = cur_img_bytes  # holds image value which gets minimized  
-        date = [] 
-        cur_char_folder = r"darkesthouraarimagefixer\%s" % char_folder
+        nonlocal cur_img_data
         
-        for charname in os.listdir(cur_char_folder):  # maybe make months into own folder so it goes faster; actually just save the HEX values
-            cur_char = Image.open(cur_char_folder + charname).tobytes()
-            index_img = cur_img_bytes.index(cur_char)
+        temp_img = cur_img_data  # editable copy
+        
+        date = [] 
+        cur_chars = chars_full if is_full else chars_lite
+
+        for i, cur_char in enumerate(cur_chars):
             while cur_char in temp_img: 
-                date.append((charname.split(".")[0], index_img))  
-                temp_img[index_img:]
-            temp_img = cur_img_bytes
+                img_index = temp_img.index(cur_char)
+                date.append(([0,1,2,3,4,5,6,7,8,9, "January", "February", "Mars", "April", "May", "June", "July", "August", "September", "October", "November", "December"][i], img_index))  
+                temp_img = temp_img[img_index:]
+            temp_img = cur_img_data
 
             if len(date) == 11: break  # early break out if maxed out on data
-        cur_img = cut_img(cur_img) 
-        return date
-   
+        cur_img = cur_img.crop((259, 76, cur_img.width, 667))  # cuts out everything except map
+        return date        
 
 def walk_folder(folder): 
     for filename in os.listdir(folder):
         cur_img = Image.open(folder+filename)
-        new_filename = "".join([x[0] for x in sorted((get_date(cur_img)), key=lambda d: d[1])])  # sorts date as: HH MM Month Day Year; currently just mashes sorted list
         infilename = os.path.join(folder,filename)
+        os.rename(infilename, infilename.split(".")[0]+".jpg")  # so that each pixel will only have a length of 3
+        new_filename = "".join([x[0] for x in sorted((get_date(cur_img)), key=lambda d: d[1])])  # sorts date as: HH MM Month Day Year; currently just mashes sorted list
         if not os.path.isfile(infilename): continue
         newname = new_filename + ".jpg" 
         os.rename(infilename, newname)
